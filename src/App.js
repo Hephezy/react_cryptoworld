605c0b3d1c29bb26c673e9cfe10d8d48cb3d1573
Hephezy <oluwapelumimartins5@gmail.com>
2022-11-09 06:09:37 +0100

    Initial Commit

---
+++ src/App.js
@@ -0,0 +1,46 @@
+import React from "react";
+import { Routes, Route, Link } from "react-router-dom";
+import { Layout, Typography, Space } from "antd";
+import { Navbar, Homepage, Cryptocurrencies, CryptoDetails, News, } from "./components";
+import './App.css';
+
+const App = () => {
+  return (
+    <div className="app">
+      <div className="navbar">
+        <Navbar />
+      </div>
+      <div className="main">
+        <Layout>
+          <div className="routes">
+            <Routes>
+              <Route exact path="/" element={<Homepage />} />
+              <Route
+                exact
+                path="/cryptocurrencies"
+                element={<Cryptocurrencies />}
+              />
+              <Route exact path="/crypto/:coinId" element={<CryptoDetails />} />
+              <Route exact path="/news" element={<News />} />
+            </Routes>
+          </div>
+        </Layout>
+        <div className="footer">
+          <Typography.Title level={5} style={{ color: 'white', textAlign: 'center' }}>Copyright © 2021
+            <Link to="/">
+              CryptoWorld Inc.
+            </Link> <br />
+            All Rights Reserved.
+          </Typography.Title>
+          <Space>
+            <Link to="/">Home</Link>
+            <Link to="/cryptocurrencies">Coins</Link>
+            <Link to="/news">News</Link>
+          </Space>
+      </div>
+    </div>
+  </div>
+ );
+};
+
+export default App;
---
+++ src/index.js
@@ -0,0 +1,19 @@
+import React from "react";
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import { BrowserRouter as Router } from 'react-router-dom';
+import 'antd/dist/antd.min.css';
+import { Provider } from "react-redux";
+import store  from './app/store';
+
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+    <Router>
+      <Provider store={store}>
+        
+          <App/>
+    
+      </Provider>
+    </Router>
+);
+
